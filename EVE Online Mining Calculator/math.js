245ec180acc5425bf52a9d8d8757e645e2da31c5
Shawn Potter <potter.shawn@comcast.net>
2019-01-08 11:07:25 -0800

    broke getSKillBonus function up, added procurer and strip miner 1

--- EVE Online Mining Calculator/math.js
+++ EVE Online Mining Calculator/math.js
@@ -3,6 +3,9 @@
  * 12/29/2018
  * math.js
  * This script file holds the nessecary math required to perform calculations
+ * TODO: Math is correct, at least for the venture.
+ *  - Start adding more ships
+ *  - 
  */
 
 
@@ -24,27 +27,49 @@ function getOutput(mining,astrogeology,frigate,barge,expedition,exhumer,ship,mod
     Break function into multiple because there
     are 9 ships that are going to need to be done
 */
-function getSkillBonus(moduleYield, mining,astrogeology,frigate,barge,expedition,exhumer,ship){
+function getSkillBonus(moduleYield, mining, astrogeology, frigate, barge, expedition, exhumer, ship){
     if(ship=="venture"){
-        var roleBonus = 2;
-        var miningBonus = 1 + (0.05 * mining);
-        var astrogeologyBonus = 1 + (0.05 * astrogeology);
-        var frigateBonus = 1 + (0.05 * frigate);
-        totalYield = moduleYield *
-                     roleBonus *
-                     miningBonus *
-                     astrogeologyBonus *
-                     frigateBonus;
-        console.log("totalYield="+totalYield.toFixed(2)); //debug
-        totalYield = totalYield.toFixed(2);
-        return totalYield;
+        venture(moduleYield, mining, astrogeology, frigate);
+    } else if(ship=="procurer"){
+        procurer(moduleYield, mining, astrogeology, frigate);
     }
+}
+function venture(moduleYield, mining, astrogeology,frigate){
+    var roleBonus = 2;
+    var miningBonus = 1 + (0.05 * mining);
+    var astrogeologyBonus = 1 + (0.05 * astrogeology);
+    var frigateBonus = 1 + (0.05 * frigate);
+    var turrets = 2;
+    totalYield = moduleYield *
+                 roleBonus *
+                 miningBonus *
+                 astrogeologyBonus *
+                 frigateBonus *
+                 turrets;
+    console.log("totalYield="+totalYield.toFixed(2)); //debug
+    totalYield = totalYield.toFixed(2);
+    return totalYield;
+}
+function procurer(moduleYield, mining, astrogeology, barge){
+    var miningBonus = 1 + (0.05 * mining);
+    var astrogeologyBonus = 1 + (0.05 * astrogeology);
+    var turrets = 2;
+    totalYield = moduleYield *
+                 miningBonus *
+                 astrogeologyBonus *
+                 turrets;
+    console.log("totalYield="+totalYield.toFixed(2)); //debug
+    totalYield = totalYield.toFixed(2);
+    return totalYield;
 }
 
 //get the base yield for the module
 function getModYield(module){
     if(module=="miner1"){
         moduleYield = 40;
+        return moduleYield;
+    } else if(module=="stripminer1"){
+        moduleYield = 675;
         return moduleYield;
     }
 }
