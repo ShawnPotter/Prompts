8d894df6b0b68d9d083c03c2044d1e06831f362e
Shawn Potter <potter.shawn@comcast.net>
2019-01-28 23:18:20 -0800

    added Alpha functionality to color picker

--- RGB to Hex/scripts/script.js
+++ RGB to Hex/scripts/script.js
@@ -7,14 +7,16 @@
  * - nothing
 */
 
+//default values for input fields
 function setDefaults(){
    document.getElementById("decRed").defaultValue="0";
    document.getElementById("decGreen").defaultValue="0";
    document.getElementById("decBlue").defaultValue="0";
 }
-
+//set the defaults
 setDefaults();
 
+//when submit button on RGB to Hex is pressed
 $("#submitRgb").on("click", function(){
    var red = "";
    var green = "";
@@ -25,17 +27,17 @@ $("#submitRgb").on("click", function(){
       red = parseInt(document.getElementById("decRed").value);
       green = parseInt(document.getElementById("decGreen").value);
       blue = parseInt(document.getElementById("decBlue").value);
-      console.log("rgb("+red+","+green+","+blue+")"); //debug
+      //console.log("rgb("+red+","+green+","+blue+")"); //debug
       
       return red, green, blue;
    }
-  
+  //convert rgb to a hex code
   function convert(red, green, blue){
       output="";
       colors = [red, green, blue];
       for(var i = 0; i <= 2; i++){
         foo = colors[i].toString(16);
-        console.log("foo="+foo);
+        //console.log("foo="+foo);
         if(foo.length % 2){
            foo = "0"+foo;
         }
@@ -44,23 +46,31 @@ $("#submitRgb").on("click", function(){
       output = "#" + output;
       return output;
    }
-   
+   //get rgb color values
    getColors();
+   //convert rgb to hex
    convert(red, green, blue);
+   
+   //set the hex code output to new value
    document.getElementById("codeOutput").setAttribute("value", output);
+   
+   //set the color preview box to new color
    document.getElementById("colorOutput").style.backgroundColor = output;
 });
 
+//submit button for Hex to RGB converter
 $("#submitHex").on("click", function(){
    var hexCode = document.getElementById("hexInput").value;
    var red = "";
    var green = "";
    var blue = "";
+   
+   //segment the hex code into three pieces and then convert to decimal
    function getRgb(){
       for(var i=0; i<=5; i++){
          if(i<2){
             red += hexCode[i];
-            console.log(red);
+            //console.log(red); //debug
          } else if(i<4){
             green += hexCode[i];
          } else{
@@ -70,14 +80,17 @@ $("#submitHex").on("click", function(){
       red = parseInt(red, 16);
       green = parseInt(green, 16);
       blue = parseInt(blue, 16);
-      console.log("rgb("+red+","+green+","+blue+")"); //debug
+      //console.log("rgb("+red+","+green+","+blue+")"); //debug
    }
+   //set values for the input boxes
    function setRgb(){
       document.getElementById("hexRed").setAttribute("value", red);
       document.getElementById("hexGreen").setAttribute("value", green);
       document.getElementById("hexBlue").setAttribute("value", blue);
       document.getElementById("colorOutput2").style.backgroundColor = "rgb("+red+","+green+","+blue+")";
    }
+   
+   //run functions
    getRgb();
    setRgb();
 });
--- RGB to Hex/scripts/slider.js
+++ RGB to Hex/scripts/slider.js
@@ -7,31 +7,38 @@
  *   - Add color hex code output for color picker
  *   - Add functionality for RGB input boxes to update sliders
 */
-$(document).ready(function(){
-    //create event listeners for all three sliders
-    document.getElementById("redSlider").addEventListener("input", updateRed);
-    document.getElementById("greenSlider").addEventListener("input", updateGreen);
-    document.getElementById("blueSlider").addEventListener("input", updateBlue);
-    
-    function updateRed(){
-         var num = document.getElementById("redSlider").value;
-         document.getElementById("redSliderNum").setAttribute("value", num);
-         updateColor();
-     }
-     function updateGreen(){
-         var num = document.getElementById("greenSlider").value;
-         document.getElementById("greenSliderNum").setAttribute("value", num);
-         updateColor();
-     }
-     function updateBlue(){
-         var num = document.getElementById("blueSlider").value;
-         document.getElementById("blueSliderNum").setAttribute("value", num);
-         updateColor();
-     }
-     function updateColor(){
-        var red = document.getElementById("redSlider").value;
-        var green = document.getElementById("greenSlider").value;
-        var blue = document.getElementById("blueSlider").value;
-        document.getElementById("colorOutput3").style.backgroundColor = "rgb("+red+","+green+","+blue+")";
-     }
-});
+//create event listeners for all three sliders
+document.getElementById("redSlider").addEventListener("input", updateRed);
+document.getElementById("greenSlider").addEventListener("input", updateGreen);
+document.getElementById("blueSlider").addEventListener("input", updateBlue);
+document.getElementById("alphaSlider").addEventListener("input", updateAlpha);
+
+//functions to update input fields for RGB values
+function updateRed(){
+     var num = document.getElementById("redSlider").value;
+     document.getElementById("redSliderNum").setAttribute("value", num);
+     updateColor();
+ }
+ function updateGreen(){
+     var num = document.getElementById("greenSlider").value;
+     document.getElementById("greenSliderNum").setAttribute("value", num);
+     updateColor();
+ }
+ function updateBlue(){
+     var num = document.getElementById("blueSlider").value;
+     document.getElementById("blueSliderNum").setAttribute("value", num);
+     updateColor();
+ }
+ function updateAlpha(){
+     var num = document.getElementById("alphaSlider").value;
+       document.getElementById("alphaSliderNum").setAttribute("value", num);
+       updateColor();
+ }
+ //function to update the color preview automatically when sliders are moved
+ function updateColor(){
+    var red = document.getElementById("redSlider").value;
+    var green = document.getElementById("greenSlider").value;
+    var blue = document.getElementById("blueSlider").value;
+    var alpha = document.getElementById("alphaSlider").value;
+    document.getElementById("colorOutput3").style.backgroundColor = "rgba("+red+","+green+","+blue+","+alpha+")";
+ }
